c9e93e547851cb1c3258d4d3760628f6e5156d7d
Harry823 <hdtjahja@gmail.com>
2025-06-16 11:33:49 -0700

    added custom local font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,26 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const notoSans = localFont({
+  src: [
+    {
+      path: './fonts/static/NotoSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/static/NotoSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/static/NotoSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    }
+  ]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={notoSans.className}
       >
         {children}
       </body>
